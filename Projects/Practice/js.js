d9fd5a30fc116967f5b1fa0680766a3e50e63bfc
Anosh Kudikala <102867566+ennu897@users.noreply.github.com>
2025-09-24 11:23:28 -0500

    fix

---
+++ Projects/Practice/js.js
@@ -0,0 +1,22 @@
+let name = "john";
+let age = 17;
+
+console.log(name);
+console.log(age);
+
+age=22;
+
+console.log(age);
+
+const pi = 3.14;
+
+// pi = 10;
+console.log(pi)
+
+let a =5;
+let b=6;
+console.log(40/59)
+
+const myInt=5;
+const myFloat=1.2;
+myInt;
