079be9571da5f59782c9d954ebdbfe247dd3c8f1
Anosh Kudikala <102867566+ennu897@users.noreply.github.com>
2025-09-28 14:54:58 -0500

    trying js exercices

--- Projects/Practice/js.js
+++ Projects/Practice/js.js
@@ -1,4 +1,4 @@
-
+/*
 // add function
 function add7(number){
     return number+7;
@@ -18,8 +18,9 @@ function lastLetter(str){
     }    
 }
 console.log(lastLetter("anoshbhk"));
+*/
 
-
+/*
 // FizzBuzz program
 let answer = parseInt(prompt("Enter a number choice of your own: "));
 for(let i=1; i<=answer; i++){
@@ -33,8 +34,13 @@ for(let i=1; i<=answer; i++){
         console.log(i);
     }
 }
+*/
 
 
+
+// // https://www.theodinproject.com/lessons/foundations-loops-and-arrays
+
+/*
 console.log("using Reduce method");
 const arr = [1, 2, 3, 4, 5];
 
@@ -63,3 +69,121 @@ console.log("using filter method");
 let ad = arr.map(all);
 console.log(ad);
 console.log(arr);
+*/
+
+
+
+// all three methods combined 
+
+/*
+const arr = [1, 2, 3, 4, 5];
+
+function sumOfTripledEvens(even){
+    return (even%2 == 0);
+}
+
+let getEven = arr.filter(sumOfTripledEvens);
+
+console.log(getEven);
+console.log(arr);
+
+let getMultiply = arr.reduce((total, currentItems) => {
+    return total*currentItems;
+}, 3);
+
+console.log(getMultiply);
+console.log(arr);
+
+let getAdd = arr.map((num) => num+3);
+
+console.log(getAdd);
+console.log(arr);
+*/
+
+
+// https://javascript.info/array-methods#tasks
+
+// Translate border-left-width to borderLeftWidth
+/* 
+function camelize(str){
+    let words = str.split("-");
+    let result = words[0];
+    for(let i=1; i< words.length; i++){
+      result += words[i].charAt(0).toUpperCase() + words[i].substring(1);
+    }
+    return result;
+  }
+
+  console.log(camelize("background-color"));
+  console.log(camelize("list-style-image"));
+  console.log(camelize("-webkit-transition"));
+
+  */
+
+
+
+//  Filter range
+/*
+let arr = [5, 3, 8, 1];
+
+function filterRange(arr, a, b){
+    function check(num){
+        return num>=a && num<=b;
+    }
+    return arr.filter(check);
+}
+
+let filtered = filterRange(arr, 1, 4);
+console.log(filtered);
+console.log(arr);
+*/
+
+
+// filter in range 
+
+/*
+let arr = [5, 3, 8, 1];
+function filterRangeInPlace(arr, a, b){
+    function check(num){
+        return num>=a && num<=b;
+    }
+    return arr.filter(check);
+}
+
+let filtered = filterRangeInPlace(arr, 1, 4);
+arr.length=0;
+arr.push(...filtered);
+
+console.log(arr);
+/*
+  let arr =[5, 3, 8, 1];
+  function filterRangeInPlace(arr, a, b){
+    return arr.filter((num) => num>=a && num<=b);
+  }
+  let filtered = filterRangeInPlace(arr, 1, 4);
+  arr.length=0;
+  arr.unshift(...filtered);
+  console.log(arr);
+*/
+
+//  sort in decreasing order
+
+
+let arr = [5, 2, 1, -10, 8];
+function decreasingOrder(arr){
+    for(let i=0; i<arr.length; i++){
+        for(let j=1; j<=arr.length-1; j++){
+            if(arr[i] > arr[j]){
+                let temp=0;
+                temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+    for(let i=0; i<arr.length; i++){
+        console.log(arr[i]);
+    }
+}
+console.log(decreasingOrder(arr));
+
