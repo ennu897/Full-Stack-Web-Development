a6f1fef651bac8bba802fb3899b4663557055bec
Anosh Kudikala <102867566+ennu897@users.noreply.github.com>
2025-09-27 20:36:06 -0500

    js

--- Projects/Practice/js.js
+++ Projects/Practice/js.js
@@ -33,3 +33,33 @@ for(let i=1; i<=answer; i++){
         console.log(i);
     }
 }
+
+
+console.log("using Reduce method");
+const arr = [1, 2, 3, 4, 5];
+
+let product = arr.reduce((total, currentItem) => {
+    return total*currentItem;
+}, 1);
+console.log(product);
+console.log(arr);
+
+
+console.log("");
+console.log("using filter method");
+    function odd(num){
+        return (num%2 != 0);
+    }
+let fil = arr.filter(odd);
+console.log(fil);
+console.log(arr);
+
+
+console.log("");
+console.log("using filter method");
+    function all(nums){
+        return nums+1;
+    }
+let ad = arr.map(all);
+console.log(ad);
+console.log(arr);
