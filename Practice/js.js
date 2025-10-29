2d50c751261bb10203efffc15a8bae5ad129c654
Anosh Kudikala <102867566+ennu897@users.noreply.github.com>
2025-10-29 11:54:44 -0500

    intial commit

--- Practice/js.js
+++ Practice/js.js
@@ -1,18 +1,50 @@
-let globalAge = 23;
+function sizeMaker(size){
+  // return () => {
+    document.body.style.fontSize = `${size}px`;
+  // }
+  
+}
+document.getElementById('size1').onclick = () => sizeMaker(12);
+document.getElementById('size2').onclick = () => sizeMaker(22);
+document.getElementById('size3').onclick = () => sizeMaker(32);
 
-function printAge(age) {
-  var varAge = 34;
+// document.getElementById('size1').onclick = sizeMaker(12);
+// document.getElementById('size2').onclick = sizeMaker(22);
+// document.getElementById('size3').onclick = sizeMaker(32);
 
-  if (age > 0) {
-    const constAge = age * 2;  // ✅ Exists only inside this block
-    console.log(constAge);     // Works fine
+
+
+
+
+/* 
+const documentMock = (() => ({
+  querySelector: (Selector) => ({
+    innerHTML: null,
+  }),
+}));
+
+
+const Formatter = ( function (doc) {
+
+  const log = (message) => console.log(`[${Date.now()}] Logger: ${message}`) ;
+
+  const makeUpperCase = (text) => {
+    log("making upper case");
+    return text.toUpperCase();
   }
 
-  // console.log(constAge);       // ❌ ReferenceError (constAge not defined)
-}
+  const writeToDOM = (Selector, message) => {
+    const upper = makeUpperCase(message);
+    doc.querySelector(Selector).innerHTML = upper;
+    return makeUpperCase(message);
+  }
 
+  return {
+    makeUpperCase, 
+    writeToDOM,
+  }
 
+})(document || documentMock)
 
-printAge(globalAge);
-// console.log(varAge);
-// console.log(constAge); 
+console.log(Formatter.writeToDOM('#target', "hello world!!"));
+ */
